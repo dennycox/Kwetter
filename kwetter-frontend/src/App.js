41128ba94739dbbfb1b61cfc7a5b92a669d5377c
Cox,Denny D <denny.cox@chello.nl>
2022-03-28 11:43:59 +0200

    Removed home page

--- kwetter-frontend/src/App.js
+++ kwetter-frontend/src/App.js
@@ -9,7 +9,6 @@ import {
 import Start from './pages/Start';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Home from './pages/Home';
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
         <Route exact path="/" component={Start} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/home" component={Home} />
       </Router>
     </div>
   );
