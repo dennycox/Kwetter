fe2885cb3fc0801e9b8ea5d0eed1f4985fd2cc2c
Cox,Denny D <denny.cox@chello.nl>
2022-05-03 15:48:44 +0200

    Started implementing api gateway

--- kwetter-frontend/src/baseUrl.js
+++ kwetter-frontend/src/baseUrl.js
@@ -3,6 +3,6 @@ var url = ''
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     url = 'http://localhost:5001';
 } else {
-    url = 'http://localhost:6060';
+    url = 'http://localhost:53695';
 }
 export default url;
